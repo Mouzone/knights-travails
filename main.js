99c454d0d3c0f3e09a400a37f7bf8617f49a0b33
Mouzone <sunnyliu010@gmail.com>
2024-08-19 12:29:55 -0400

    Execute and test

--- main.js
+++ main.js
@@ -1,1 +1,43 @@
-console.log("hello world")
+function knightMoves(start, end) {
+    const moves = [
+        [2, 1], [2, - 1], [-2, 1], [-2, -1],
+        [1, 2], [1, -2], [-1, 2], [-1, -2]
+    ]
+
+    const queue = [[start]]
+    const visited = new Set()
+    visited.add(start.toString())
+
+    while (queue.length) {
+        const path = queue.shift()
+        const [x, y] = path[path.length - 1]
+
+        if (x === end[0] && y === end[1]) {
+            printShortestPath(path)
+            return
+        }
+
+        moves.forEach(([dx, dy]) => {
+            const move = [x + dx, y + dy]
+            if (!visited.has(move.toString()) &&
+                checkLegalMove(x + dx, y + dy)) {
+                
+                visited.add(move.toString())
+                queue.push([...path, move])
+            }
+        })
+    }
+}
+
+function printShortestPath(answer){
+    console.log(`You made it in ${answer.length - 1} moves! Here's your path:`)
+    answer.forEach(move => {
+        console.log(move)
+    })
+}
+
+function checkLegalMove(x, y){
+    return (x > -1 && x < 8 && y > -1 && y < 8)
+}
+
+knightMoves([3,3], [4,3])
